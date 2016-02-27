c0581ad271cc551e3c824187bea9ccfb05d2eace
Aworlde <almir_extreme@hotmail.it>
2016-02-27 18:36:41 +0100

    New Background

--- love2d/background.lua
+++ love2d/background.lua
@@ -1,18 +1,44 @@
-function createBackground (x, y, pictureName) 
+function createBackground (x, y, pictureName, speed) 
 
   local obj = {}
   obj.x = x
   obj.y = y
 
   obj.image = love.graphics.newImage(pictureName)
+  obj.speed = speed
+  
+  obj.update = function(dt, xFigt1, xFigt2)
+    canMoveBackVar = canMoveBack(xFigt1, xFigt2)
+    if canMoveBackVar == true then
+      if love.keyboard.isDown("a") then
+        obj.x = obj.x + dt * obj.speed
+      end
 
-
-  obj.draw = function()
+      if love.keyboard.isDown("d") then
+        obj.x = obj.x - dt * obj.speed
+      end
       
-      love.graphics.draw(obj.image, obj.x, obj.y)
-      love.graphics.rectangle("fill", 20, 50, 60, 120 )
+      --[[
+      obj.x = math.max(-10, obj.x)
+      obj.x = math.min(love.graphics.getWidth() + 50, obj.x)
+      ]]--
       
     end
-return obj
-
+		
+	end
+  
+  obj.draw = function()      
+      love.graphics.draw(obj.image, obj.x, obj.y)
+      --love.graphics.rectangle("fill", 20, 50, 60, 120 )
+  end    
+  
+ return obj  
 end
+
+function canMoveBack (x1, x2) 
+    if (x1 > 0 and x1 < love.graphics.getWidth()-33) and (x2 > 0 and x2 < love.graphics.getWidth()-65) then
+      return true
+    else
+      return false
+    end
+  end
--- love2d/main.lua
+++ love2d/main.lua
@@ -1,9 +1,24 @@
 require('lib/32linesofgoodness')
 require('background')
+require('clouds')
 require('fighter')
 
 function love.load()
-  background = createBackground(0,0, 'gfx/back01.png')  
+ 
+  background = createBackground(-100,-50, 'gfx/back01.png', 10)   
+  street = createBackground(-100,love.graphics.getHeight()- 60 , 'gfx/street01.jpg', 70)   
+  street2 = createBackground(450,love.graphics.getHeight()- 60 , 'gfx/street01.jpg', 70)   
+  castel = createBackground(love.graphics.getWidth() - love.graphics.getWidth()/2 +25 ,love.graphics.getHeight()-   love.graphics.getHeight()/2 - 50 , 'gfx/castel01.png', 25) 
+  
+  cloud1= createBackground(10,50 , 'gfx/cloud.png', 50)  
+  cloud2= createBackground(150,90 , 'gfx/cloud.png', 50)  
+  cloud3= createBackground(300,30 , 'gfx/cloud.png', 50)  
+  cloud4= createBackground(500,70 , 'gfx/cloud.png', 50)  
+  cloud5= createBackground(800,10 , 'gfx/cloud.png', 50)  
+
+  clouds = {cloud1, cloud2, cloud3, cloud4, cloud5}
+  backgrounds = {background, street, street2, castel}
+ 
 	fighter1 = createFighter(8, love.graphics.getHeight() - 32, 32, 32, 100)
 	fighter2 = createFighter(448, love.graphics.getHeight() - 64, 64, 64, 200)
 
@@ -12,11 +27,28 @@ end
 function love.update(dt)
 	fighter1.update(dt)
 	fighter2.update(dt)
+   
+  
+  for k,v in pairs (backgrounds) do
+    v.update(dt, fighter1.x, fighter2.x)
+  end
+  for k,v in pairs (clouds) do
+    v.update(dt, fighter1.x, fighter2.x)
+  end
+  
+  animateClouds(dt, clouds)
+  
 end
 
 function love.draw()
-  background.draw()
-    --haus.draw()
+  local i=0
+  for k,v in pairs (backgrounds) do
+    v.draw()
+  end
+  for k,v in pairs (clouds) do
+    v.draw()
+  end
+  
 	fighter1.draw()
 	fighter2.draw()
 end
