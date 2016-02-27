3b9e7c0eeddb335ad9e6888e8bd5c1f2d3d58642
Marcus Ihde <marcusplate@googlemail.com>
2016-02-27 12:29:46 +0100

    init project.

---
+++ love2d/main.lua
@@ -0,0 +1,27 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.mousepressed(x, y, button)
+
+end
+
+function love.mousemoved(x, y, dx, dy)
+
+end
