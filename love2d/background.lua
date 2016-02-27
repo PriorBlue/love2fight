f160282a044eda0fc98eb07404143053144573d1
Aworlde <almir_extreme@hotmail.it>
2016-02-27 19:57:36 +0100

    Quad

--- love2d/background.lua
+++ love2d/background.lua
@@ -1,21 +1,33 @@
-function createBackground (x, y, pictureName, speed) 
+function createBackground (x, y, pictureName, speed, isQuad, width, height) 
 
   local obj = {}
   obj.x = x
   obj.y = y
-
+  obj.px = 0
+  
   obj.image = love.graphics.newImage(pictureName)
+
+  obj.isQuad = isQuad or false
+  obj.width = width or obj.image:getWidth()
+  obj.height = height or obj.image:getHeight()
+  
+  
+  obj.image:setWrap("repeat", "repeat")
   obj.speed = speed
+  
+   
+  obj.Quad= love.graphics.newQuad (obj.x, obj.y, obj.width, obj.height,  obj.image:getWidth(), obj.image:getHeight()) 
+  --obj.Quad:setViewport( x, y, w, h )
   
   obj.update = function(dt, xFigt1, xFigt2)
     canMoveBackVar = canMoveBack(xFigt1, xFigt2)
     if canMoveBackVar == true then
       if love.keyboard.isDown("a") then
-        obj.x = obj.x + dt * obj.speed
+        obj.px = obj.px + dt * obj.speed
       end
 
       if love.keyboard.isDown("d") then
-        obj.x = obj.x - dt * obj.speed
+        obj.px = obj.px - dt * obj.speed
       end
       
       --[[
@@ -28,8 +40,12 @@ function createBackground (x, y, pictureName, speed)
 	end
   
   obj.draw = function()      
-      love.graphics.draw(obj.image, obj.x, obj.y)
-      --love.graphics.rectangle("fill", 20, 50, 60, 120 )
+    if obj.isQuad then
+    obj.Quad:setViewport( obj.px, 0,  obj.width, obj.height)
+      love.graphics.draw(obj.image, obj.Quad, obj.x, obj.y)
+    else
+      love.graphics.draw(obj.image, obj.x - obj.px, obj.y)
+    end
   end    
   
  return obj  
