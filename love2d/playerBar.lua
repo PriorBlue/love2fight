d5100b3cae9d38338ca7af0637aeffc4ab58b175
namenodreamz <namenodreamz@gmail.com>
2016-02-27 19:09:42 +0100

    Added Healthbars with healb status in Different Color

--- love2d/playerBar.lua
+++ love2d/playerBar.lua
@@ -1,30 +1,46 @@
 local g = require('love.graphics')
 
 class "PlayerBar" {
-    isPlayerOne = false;
+    
 }
 
-function PlayerBar:__init(isPlayerOne, health)
-    self.isPlayerOne = isPlayerOne or false
-    self.maxHealth = health
-    self.health = health
+function PlayerBar:__init(isPlayerOne, maxhealth, xPos, yPos, width, height)
+    self.maxHealth   = 100
+    self.health      = 90
+    self.xPos        = xPos
+    self.yPos        = yPos
+    self.width       = width
+    self.height      = height
+    self.isPlayerOne = isPlayerOne
 end
 
 function PlayerBar:draw()
-    local oldr,oldg,oldb,olda = g.getColor()
-    if self.isPlayerOne then
-        g.setColor(200,0,0,200)
-        g.rectangle("fill",0,20,100,100)
-    else
-        g.setColor(200,0,0,200)
-        g.rectangle("fill",450,20,100,100)
-    end
-    -- draw health bar
-    --TODO
+    local oldr,oldg,oldb,olda = g.getColor()	   
     
-    -- draw lives
-    --TODO
+    -- draw Max XP
+    g.setColor(100,100,100,150)
+    g.rectangle("fill", self.xPos, self.yPos, self.width, self.height) 
+
+    -- Calculate Actual Width
+    local factor = self.health / self.maxHealth
+    local actualHealthbarWidth = self.width * factor
+
+    --  draw 
+    if self.isPlayerOne then  --left
+        local actualHealthbarXPos = (self.width - actualHealthbarWidth + self.xPos);
+        g.setColor(0,255,0,200)
+        g.rectangle("fill", actualHealthbarXPos, self.yPos, actualHealthbarWidth, self.height)        
+    else --right
+       
+        g.setColor(0,255,0,200)       
+        g.rectangle("fill", self.xPos, self.yPos, actualHealthbarWidth, self.height) 
+    end
+
     
     g.setColor(oldr,oldg,oldb,olda)
+end
+
+function PlayerBar:update(dt,health)
+    self.health = health
 end
 
