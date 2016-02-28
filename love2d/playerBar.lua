1db768d794be7e2492d8ba2209c0116e8dc0d95f
namenodreamz <namenodreamz@gmail.com>
2016-02-28 12:08:04 +0100

    Fixed Health Bars

--- love2d/playerBar.lua
+++ love2d/playerBar.lua
@@ -7,11 +7,22 @@ class "PlayerBar" {
 function PlayerBar:__init(isPlayerOne, maxhealth, xPos, yPos, width, height)
     self.maxHealth   = 100
     self.health      = 90
+   
     self.xPos        = xPos
     self.yPos        = yPos
     self.width       = width
     self.height      = height
+
+    self.xPosHealth  = xPos
+    self.yPosHealth  = yPos
+    self.widthHealth = width    
+    self.heightHealth = height    
+
     self.isPlayerOne = isPlayerOne
+    self.hbColor = {0,255,0,220}   
+
+    self.xPosDamageBarP1 = 0
+    self.widthDamangeBarP2 = 0
 end
 
 function PlayerBar:draw()
@@ -23,24 +34,45 @@ function PlayerBar:draw()
 
     -- Calculate Actual Width
     local factor = self.health / self.maxHealth
-    local actualHealthbarWidth = self.width * factor
+    self.widthHealth = self.width * factor
 
     --  draw 
-    if self.isPlayerOne then  --left
-        local actualHealthbarXPos = (self.width - actualHealthbarWidth + self.xPos);
-        g.setColor(0,255,0,200)
-        g.rectangle("fill", actualHealthbarXPos, self.yPos, actualHealthbarWidth, self.height)        
-    else --right
-       
-        g.setColor(0,255,0,200)       
-        g.rectangle("fill", self.xPos, self.yPos, actualHealthbarWidth, self.height) 
+    if self.isPlayerOne then  
+        self.xPosHealth = (self.width - self.widthHealth + self.xPos);
+        g.setColor(self.hbColor[1],self.hbColor[2],self.hbColor[3],self.hbColor[4])
+        g.rectangle("fill", self.xPosHealth, self.yPosHealth, self.widthHealth, self.heightHealth)        
+    else                
+        g.setColor(self.hbColor[1],self.hbColor[2],self.hbColor[3],self.hbColor[4])       
+        g.rectangle("fill", self.xPosHealth, self.yPosHealth, self.widthHealth, self.heightHealth) 
     end
 
+    -- draw the damaged Health at the end with the faded Color
     
+    if oldHealthFadingValue ~= 0 then
+      g.setColor(self.hbColor[1],self.hbColor[2],self.hbColor[3],self.hbColor[4]) 
+      --g.rectangle("fill", xPos,y,widht,height)       
+    end
     g.setColor(oldr,oldg,oldb,olda)
 end
 
-function PlayerBar:update(dt,health)
-    self.health = health
+function PlayerBar:update(dt,newHealth)
+    
+    if newHealth < self.health then 
+        self.health = newHealth
+                   
+        self.xPosDamageBar = xPosHealth  --Backup old xPos
+        
+            
+        
+    else
+        self.health  = newHealth
+    end
+
+    --Start the the fading timer
+
+end
+
+function PlayerBar:calculateirgendwas()
+
 end
 
